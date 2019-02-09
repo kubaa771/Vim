96660c657674ec15767b08c95fa6045308ceebf9
Jakub Iwaszek <kubaa771@gmail.com>
2019-02-09 19:46:51 +0100

    initial

---
+++ Vim/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vim
+//
+//  Created by Jakub Iwaszek on 09/02/2019.
+//  Copyright © 2019 Jakub Iwaszek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
